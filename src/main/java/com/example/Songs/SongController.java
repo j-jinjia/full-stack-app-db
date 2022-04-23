db5c4b1de9d835f078cfeb1d09d8f0551f187c78
Jiazheng Jin Jia <j.jinjia99@gmail.com>
2022-04-24 00:23:53 +0200

    cross origin issue

--- src/main/java/com/example/Songs/SongController.java
+++ src/main/java/com/example/Songs/SongController.java
@@ -1,12 +1,19 @@
 package com.example.Songs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
+
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
+@CrossOrigin
 @RestController
 public class SongController {
     @Autowired
@@ -16,9 +23,27 @@ public class SongController {
     public ResponseEntity<List<Song>> getSongs(){
         return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
     }
-    @PostMapping("/api/song")
+    @GetMapping("/song/{id}")
+    public ResponseEntity<Song> getSongById(@PathVariable String id) {
+        for (Song song : repository.findAll()) {
+            if (song.getId() == parseInt(id)) {
+                return ResponseEntity.status(HttpStatus.OK).body(song);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); //return 404 if the greeting wasn't found
+    }
+    @PostMapping("/song/add")
     public ResponseEntity<Song> createSong(@RequestBody Song song){
         repository.save(song);
         return ResponseEntity.status(HttpStatus.CREATED).body(song);
+    }
+    @DeleteMapping("/song/delete/{id}")
+    public ResponseEntity<String> deleteSong(@PathVariable String id) {
+        try {
+            repository.deleteById(parseInt(id));
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Song with id of: " + id + " was deleted");
+        } catch (EmptyResultDataAccessException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Song with the id of :" + id + "doesn't exist, can't delete!");
+        }
     }
 }
