be87731f140f32e37eda392c6f67c33dc82b5462
Jiazheng Jin Jia <j.jinjia99@gmail.com>
2022-04-24 01:27:05 +0200

    attempt 6

--- src/main/java/com/example/Songs/SongsApplication.java
+++ src/main/java/com/example/Songs/SongsApplication.java
@@ -2,7 +2,9 @@ package com.example.Songs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 
 @SpringBootApplication
 
