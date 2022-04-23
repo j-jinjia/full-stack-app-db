3ded1b27190fecab3f7d7074a0bca0cd6c67fc7c
Jiazheng Jin Jia <j.jinjia99@gmail.com>
2022-04-24 01:21:45 +0200

    attempt 5 cross origin

--- src/main/java/com/example/Songs/SongController.java
+++ src/main/java/com/example/Songs/SongController.java
@@ -12,7 +12,7 @@ import static java.lang.Integer.parseInt;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class SongController {
     @Autowired
     SongRepository repository;
